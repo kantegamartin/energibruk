5a1acd9b293cdaba836a83800378026f8c43ac49
Martin Larsson <martin.larsson@kantega.no>
2025-03-26 21:54:23 +0100

    Basified version of ArjanW's code

--- java/src/main/java/no/kantega/obrc/Solution.java
+++ java/src/main/java/no/kantega/obrc/Solution.java
@@ -1,72 +1,103 @@
+/*
+ *  Copyright 2023 The original authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package no.kantega.obrc;
 
-import java.io.IOException;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collector;
+import java.util.*;
 
-import static java.util.stream.Collectors.groupingBy;
 
 public class Solution {
-
     private static final String FILE = "./measurements.txt";
 
-    private record Measurement(String station, double value) {
-        private Measurement(String[] parts) {
-            this(parts[0], Double.parseDouble(parts[1]));
-        }
-    }
-
-    private record ResultRow(double min, double mean, double max) {
-        public String toString() {
-            return round(min) + "/" + round(mean) + "/" + round(max);
-        }
-
-        private double round(double value) {
-            return Math.round(value * 10.0) / 10.0;
-        }
-    };
-
-    private static class MeasurementAggregator {
-        private double min = Double.POSITIVE_INFINITY;
-        private double max = Double.NEGATIVE_INFINITY;
-        private double sum;
-        private long count;
-    }
-
-    public static void main(
-            String[] args
-            ) throws IOException
+    public static void main(String[] args) throws Exception
     {
-        String file = args.length > 0 ? args[0] : FILE;
-        Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
-                MeasurementAggregator::new,
-                (a, m) -> {
-                    a.min = Math.min(a.min, m.value);
-                    a.max = Math.max(a.max, m.value);
-                    a.sum += m.value;
-                    a.count++;
-                },
-                (agg1, agg2) -> {
-                    var res = new MeasurementAggregator();
-                    res.min = Math.min(agg1.min, agg2.min);
-                    res.max = Math.max(agg1.max, agg2.max);
-                    res.sum = agg1.sum + agg2.sum;
-                    res.count = agg1.count + agg2.count;
+        var file = new File(args.length > 0 ? args[0] : FILE);
+        var results = parse(file).toStringArray();
+        Arrays.sort(results);
+        System.out.println(String.join("\n", results));
+    }
 
-                    return res;
-                },
-                agg -> {
-                    return new ResultRow(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max);
-                });
+    private static StationList parse(File file) throws Exception {
+        StationList stationList = new StationList();
+        Files.lines(Paths.get(file.getAbsolutePath()))
+                .map(l -> new Station(l.split(";")))
+                .forEach(stationList::add);
+        return stationList;
+    }
 
-        Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(file))
-                .map(l -> new Measurement(l.split(";")))
-                .parallel()
-                .collect(groupingBy(m -> m.station(), collector)));
+    private static final class Station {
+        private final String name;
+        private double min;
+        private double max;
+        private double total;
+        private double count;
 
-        System.out.println(measurements);
+        public Station(String[] parts)
+        {
+            name = parts[0];
+            min = max = total = Double.parseDouble(parts[1]);
+            count = 1;
+        }
+
+        @Override
+        public String toString() {
+            return name + "=" + min + "/" + Math.round(10.0 * total / count) / 10.0 + "/" + max;
+        }
+
+        private void append(double min, double max, double total, double count) {
+            if (min < this.min)
+                this.min = min;
+            if (max > this.max)
+                this.max = max;
+            this.total += total;
+            this.count += count;
+        }
+
+        public void merge(Station other) {
+            append(other.min, other.max, other.total, other.count);
+        }
+    }
+
+    private static class StationList implements Iterable<Station> {
+        private final Map<String, Station> array = new HashMap<>();
+
+        public void add(Station station) {
+            var existing = array.get(station.name);
+            if (existing == null) {
+                array.put(station.name, station);
+            } else {
+                existing.merge(station);
+            }
+        }
+
+        public String[] toStringArray() {
+            var destination = new String[array.size()];
+
+            var i = 0;
+            for (Station station : this)
+                destination[i++] = station.toString();
+
+            return destination;
+        }
+
+        @Override
+        public Iterator<Station> iterator() {
+            return array.values().iterator();
+        }
     }
 }
